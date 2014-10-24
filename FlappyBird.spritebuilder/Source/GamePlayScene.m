4d7d0e711b9acc4d85d98f2105b86ff5905a7d52
Hans Vila <HansVila@MBP-di-Hans.homenet.telecomitalia.it>
2014-10-24 15:19:22 +0200

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,13 +14,14 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+[self addObstacle];
+timeSinceObstacle = 0.0f;
 }
 
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
 [character flap];
-    [self addObstacle];
 }
 
 @end
